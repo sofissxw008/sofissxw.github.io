4ea0e307e1f89717d0465e2e4d0715ae6a2872e5
sofissxw008 <sofhiebatista8@gmail.com>
2026-02-15 19:17:13 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,20 @@
+let indiceAtual = 0;
+
+function verificarSenha() {
+    const senhaCorreta = "1909";
+    const senhaDigitada = document.getElementById("senha").value;
+
+    if (senhaDigitada === senhaCorreta) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("conteudo").style.display = "block";
+    } else {
+        alert("Dica: tenta sem o ano, apena o dia e o mês")
+    }
+}
+
+function proximo() {
+    const blocos = document.querySelectorAll(".bloco");
+    blocos[indiceAtual].classList.remove("ativo");
+    indiceAtual++;
+    blocos[indiceAtual].classList.add("ativo");
+}
